718316e95faa7150fa907258f8a2dd262dd0248a
Henry Calderon <theceintiant@gmail.com>
2022-08-30 09:02:59 -0700

    Starting Off

---
+++ Primary-Review/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Primary-Review
+//
+//  Created by Henry Calderon on 8/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Primary-ReviewUITests/Primary_ReviewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Primary_ReviewUITestsLaunchTests.swift
+//  Primary-ReviewUITests
+//
+//  Created by Henry Calderon on 8/30/22.
+//
+
+import XCTest
+
+class Primary_ReviewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
